70ba1d3ebbd9b5cf2084ee1ffef5c4904b3bbbed
zhin abubakr <zheen99abubakr@gmail.com>
2024-05-02 20:32:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+  const thankYouMessages = [];
+  for (let name of names) {
+    thankYouMessages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  return thankYouMessages;
+}
+
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
